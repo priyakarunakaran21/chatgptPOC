e126692f2a021333ea80b83ac1d197396c716644
Priyadharshini Karunakaran <Priyadharshinik@emids.com>
2023-05-22 17:31:04 +0530

    changes for new ui

--- chatgpt-poc/src/component/Chat/index.js
+++ chatgpt-poc/src/component/Chat/index.js
@@ -1,80 +1,99 @@
-import { useState, useRef, useEffect} from 'react';
-import Modal from '../Modal/index'
-import './style.scss'
+import { useState, useRef, useEffect } from 'react';
+
+import './style.scss';
 import Sidebar from './sidebar';
 const Chat = ({messages}) => {
-  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(true);
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
+  const [conversationHistory, setConversationHistory] = useState('');
+  const [savedConversationHistory, setSavedConversationHistory] = useState('');
+  const [chatSessions, setChatSessions] = useState([]);
 
   const toggleSidebar = () => {
     setIsSidebarCollapsed(!isSidebarCollapsed);
   };
-    const apiKey = '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';
-    const myRef = useRef(null);
+  const myRef = useRef(null);
+  let convoSession = '';
+  let newbotHistory1 = '';
+  const newbotHistory1Ref = useRef('');
+  const [regenerateCount, setRegenerateCount] = useState(0);
+  const [regenerate, setRegenerate] = useState(false);
+  let htmlCode;
+  const conversationHistoryRef = useRef('');
+  const promptRef = useRef('');
+  const [prompt, setPrompt] = useState('');
+  const [chatbotResponse, setChatbotResponse] = useState('');
+  const [intro, setIntro] = useState(true);
+  
+  useEffect(() => {
+    if (regenerateCount > 0) {
+      regenerateResponses();
+    }
+  }, [regenerateCount]);
+
+  const startNewSession = () => {
+    debugger;
+    if(chatbotResponse !== ""){
     let convoSession = '';
-    let conversationHistory = '';
     let newbotHistory1 = '';
-    const newbotHistory1Ref = useRef('');
-    const [regenerateCount, setRegenerateCount] = useState(0);
-    const [regenerate, setRegenerate] = useState(false);
-    let htmlCode;
-    const conversationHistoryRef = useRef('');
-    const promptRef = useRef('');
+    conversationHistoryRef.current = '';
+    setConversationHistory('');
+    setChatbotResponse('');
+    setIntro(true);
+    setChatSessions((prevSessions) => [...prevSessions, chatbotResponse]);
+  }else{alert("New window is already open")}
+  };
   
-    // const [conversationHistory, setConversationHistory] = useState('');
-    const [prompt, setPrompt] = useState('');
-    const [chatbotResponse, setChatbotResponse] = useState('');
-    const [intro, setIntro] = useState(true);
-    const [showModal, setShowModal] = useState(false); 
+  const savedHistory1 = (index) => {
+    //console.log(savedConversationHistory);
     
-    useEffect(() => {
-      if (regenerateCount > 0) {
-        regenerateResponses();
-      }
-    }, [regenerateCount]);
-  
-    
-      const regenerateResponses = async () => {
-        try {
-        //   const response = await fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
-        //   method: 'POST',
-        //   headers: {
-        //     'Content-Type': 'application/json',
-        //     'Authorization': `Bearer ${apiKey}`,
-        //   },
-        //   body: JSON.stringify({
-        //     prompt: newbotHistory1Ref.current,
-        //     max_tokens: 500,
-        //     n: 1,
-        //   }),
-        // });
-          const formData = new FormData();
+    setIntro(false);
+    conversationHistoryRef.current = chatSessions[index];
+    setChatbotResponse(chatSessions[index]);
+  };
+
+  //console.log(savedConversationHistory);
+  const regenerateResponses = async () => {
+    try {
+      // const response = await fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //     'Authorization': `Bearer ${apiKey}`,
+      //   },
+      //   body: JSON.stringify({
+      //     prompt: newbotHistory1Ref.current,
+      //     max_tokens: 500,
+      //     n: 1,
+      //   }),
+      // });
+      const formData = new FormData();
                   formData.append('input_text', newbotHistory1Ref.current);
                   formData.append('task', 'deidentify_summarize_chatgpt');
         
-                  const response = await fetch('http://13.127.239.11/deid/messages', {    
+                  const response = await fetch('http://13.232.63.102/deid/messages', {    
                     method: 'POST',
                     body: formData
                   });
     
           const data = await response.json();
-          const generatedText = data.chatgpt.trim();
+          const generatedText = data.summary_chatgpt.trim();
 
-          if (isCode(generatedText)) {
-            // ${chatbotMessage.replace(/\n/g, '<br />').replace(/`([^!]+)`/g, '<pre><code>$1</code></pre>')}
-            htmlCode= `<div class="bot-chat"><span class="bot"></span><div class="message-bubble"><pre><code>${generatedText.replace(/\n/g, '<br />')}</code></pre></div></div>\n\n`;
-          } else {
-            htmlCode = `<div class="bot-chat"><span class="bot"></span><div class="message-bubble">${generatedText.replace(/\n/g, '<br />')}</div></div>\n\n`;
-          }
+     if (isCode(generatedText)) {
+              // ${chatbotMessage.replace(/\n/g, '<br />').replace(/`([^!]+)`/g, '<pre><code>$1</code></pre>')}
+              htmlCode= `<div class="bot-chat"><span class="bot"></span><div class="message-bubble"><pre><code>${generatedText.replace(/\n/g, '<br />')}</code></pre></div></div>\n\n`;
+            } else {
+              htmlCode = `<div class="bot-chat"><span class="bot"></span><div class="message-bubble">${generatedText.replace(/\n/g, '<br />')}</div></div>\n\n`;
+            }
 
           if (response.ok) {
-           const deidMessage = data.deidentified.trim();
-           const chatbotMessage = data.chatgpt.trim();
+            const deidMessage = data.deidentified.trim();
+           const chatbotMessage = data.summary_chatgpt.trim();
             const newbotHistory = `${conversationHistoryRef.current}  ${chatbotMessage}\n\n`;
             conversationHistoryRef.current = newbotHistory;
-            const newbotSession = `${htmlCode}\n\n`;
-            //const newbotSession = `${htmlCode}\n\n`;
+            const newbotSession =  `${htmlCode}\n\n`;
             setChatbotResponse((prevText) => prevText.concat(newbotSession));
-
+            //console.log(chatbotResponse);
+            //console.log(newbotSession);
           } else {
             throw new Error(data.error.message);
           }
@@ -84,43 +103,38 @@ const Chat = ({messages}) => {
     
         scrollToBottom();
       };
-    const scrollToBottom = () => {
+      const scrollToBottom = () => {
         setTimeout(function () {
-            myRef.current.scrollIntoView(false);
+          myRef.current.scrollIntoView(false);
         }, 10);
       };
-
-    const handleInfoIconClick = () => {
-      setShowModal(true); 
-    };
     
-    const handleModalClose = () => {
-      setShowModal(false); 
-    };
+     
     
-    function handleChange(event) {
-      setPrompt(promptRef.current.value);
-    }
-    
-    function handleKeyPress(event) {
-      if (event.keyCode === 13) {
-        handleEnter();
+      function handleChange(event) {
+        setPrompt(promptRef.current.value);
       }
-    }
     
-    function isCode(text) {
-      return /^\s/.test(text) || text.startsWith("import") || text.startsWith("function") || text.startsWith("//Function") || text.startsWith("public");
-    }
-  
-    const handleEnter = async () => {
+      function handleKeyPress(event) {
+        if (event.keyCode === 13) {
+          handleEnter();
+        }
+      }
+    
+      function isCode(text) {
+        return /^\s/.test(text) || text.startsWith("import") || text.startsWith("function") || text.startsWith("//Function") || text.startsWith("public");
+      }
+    
+      const handleEnter = async () => {
         setIntro(false);
         const newHistory = `${conversationHistoryRef.current} You: ${prompt}\n`;
         conversationHistoryRef.current = newHistory;
         newbotHistory1Ref.current = newHistory;
-        
+    
         const convoSession = `<div class="person-chat"><span class="user"></span><label class="user-message-bubble">${prompt}\n</label></div>`;
+        const newChatSession = `${chatbotResponse}${convoSession}`;
         setChatbotResponse((prevText) => prevText.concat(convoSession));
-      
+    
         try {
           // const response = await fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
           //   method: 'POST',
@@ -132,47 +146,33 @@ const Chat = ({messages}) => {
           //     prompt: conversationHistoryRef.current,
           //     max_tokens: 500,
           //     n: 1,
+          //     temperature: 0.2
           //   }),
           // });
-
             const formData = new FormData();
-          formData.append('input_text', conversationHistoryRef.current);
-          formData.append('task', 'deidentify_summarize_chatgpt');
+                    formData.append('input_text', conversationHistoryRef.current);
+                    formData.append('task', 'deidentify_summarize_chatgpt');
+          
+                    const response = await fetch('http://13.232.63.102/deid/messages', {    
+                      method: 'POST',
+                      body: formData
+                    });
+          
+                   const data = await response.json();
+                   debugger
+    
+                   const generatedText = data.summary_chatgpt.trim();
 
-          const response = await fetch('http://13.127.239.11/deid/messages', {    
-            method: 'POST',
-            body: formData
-          });
-
-         
-
-         const data = await response.json();
-         debugger
-        // data.choices.forEach((choice) => {
-        //   const generatedText = choice.text.trim();
-
-        //   if (isCode(generatedText)) {
-        //     // ${chatbotMessage.replace(/\n/g, '<br />').replace(/`([^!]+)`/g, '<pre><code>$1</code></pre>')}
-        //     htmlCode= `<div class="bot-chat"><span class="bot"></span><div class="message-bubble"><pre><code>${generatedText.replace(/\n/g, '<br />')}</code></pre></div></div>\n\n`;
-        //   } else {
-        //     htmlCode = `<div class="bot-chat"><span class="bot"></span><div class="message-bubble">${generatedText.replace(/\n/g, '<br />')}</div></div>\n\n`;
-        //   }
-        // });
-        
-       
-          const generatedText = data.chatgpt.trim();
-
-          if (isCode(generatedText)) {
-            // ${chatbotMessage.replace(/\n/g, '<br />').replace(/`([^!]+)`/g, '<pre><code>$1</code></pre>')}
-            htmlCode= `<div class="bot-chat"><span class="bot"></span><div class="message-bubble"><pre><code>${generatedText.replace(/\n/g, '<br />')}</code></pre></div></div>\n\n`;
-          } else {
-            htmlCode = `<div class="bot-chat"><span class="bot"></span><div class="message-bubble">${generatedText.replace(/\n/g, '<br />')}</div></div>\n\n`;
-          }
-        
+                   if (isCode(generatedText)) {
+                     // ${chatbotMessage.replace(/\n/g, '<br />').replace(/`([^!]+)`/g, '<pre><code>$1</code></pre>')}
+                     htmlCode= `<div class="bot-chat"><span class="bot"></span><div class="message-bubble"><pre><code>${generatedText.replace(/\n/g, '<br />')}</code></pre></div></div>\n\n`;
+                   } else {
+                     htmlCode = `<div class="bot-chat"><span class="bot"></span><div class="message-bubble">${generatedText.replace(/\n/g, '<br />')}</div></div>\n\n`;
+                   }
+    
           if (response.ok) {
-          // const chatbotMessage = data.choices[0].text.trim();
-           const deidMessage = data.deidentified.trim();
-           const chatbotMessage = data.chatgpt.trim();
+            const deidMessage = data.deidentified.trim();
+           const chatbotMessage = data.summary_chatgpt.trim();
             const newbotHistory = `${conversationHistoryRef.current}  ${chatbotMessage}\n\n`;
             conversationHistoryRef.current = newbotHistory;
             const newbotSession = `<div class="de-bot-chat"><span class="de-bot"></span><div class="de-message-bubble">${deidMessage.replace(/\n/g, '<br />')}</div></div>\n\n ${htmlCode}\n\n`;
@@ -184,80 +184,54 @@ const Chat = ({messages}) => {
         } catch (error) {
           setChatbotResponse(`Error: ${error.message}`);
         }
-    setRegenerate(false)
-    scrollToBottom()
-    setPrompt("")
-    };
+        //setChatbotResponse(newChatSession);
+        setRegenerate(false);
+        scrollToBottom();
+        setPrompt("");
+      };
+    
 
-  
-    return (
-      <div className="App">
-        <span className="info-circle" onClick={handleInfoIconClick}>i</span>
-        <div className="chat-wrapper">
-        <div className={`sidebar ${isSidebarCollapsed ? 'collapsed' : ''}`}>
-          {/* Sidebar content here */}
-          <button className="toggle-button" onClick={toggleSidebar}>
-          <svg xmlns="http://www.w3.org/2000/svg"  height="24px" id="Layer_1" version="1.1" viewBox="0 0 32 32" width="24px"><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"/></svg>
-          </button>
-          <Sidebar isShow={!isSidebarCollapsed}/>
-        </div>
-
-          <div className={`chat-container ${!isSidebarCollapsed ? 'shrink' : ''}`} ref={myRef}>
-         
-          {intro ?
-              <div className="message-thread">
-                <div className="center-align">
+      return (
+        <div className="App">
+          
+          <div className="chat-wrapper">
+            <div className={`sidebar ${isSidebarCollapsed ? 'collapsed' : ''}`}>
+              {/* Sidebar content here */}
+              {/* <button className="toggle-button" onClick={toggleSidebar}>
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" id="Layer_1" version="1.1" viewBox="0 0 32 32" width="24px"><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"/></svg>
+              </button> */}
+              <Sidebar isShow={!isSidebarCollapsed} startNewSession={startNewSession} savedHistory1={savedHistory1} chatSessions={chatSessions}/>
+            </div>
+            <div className={`chat-container ${!isSidebarCollapsed ? 'shrink' : ''}`} ref={myRef}>
+              {intro ? (
+                <div className="message-thread">
+                  <div className="center-align">
                     <h1>Welcome to ChattyMind!</h1>
                     <p>Click send to start the conversation</p>
-            
-            </div>
-           </div>
-            :
-  
-        //      <div className="message-thread">
-        //      {chatbotResponse.split('\n').map((str, index) => (
-        //    <p className="message" key={index} dangerouslySetInnerHTML={{ __html: str }}>
-        //      {/* {str}
-        //      {index !== chatbotResponse.split('\n').length - 1 && <br />} */}
-        //    </p>
-        //  ))}
-        //  </div>
-        <div className="message-thread">
-         <div className="convo-wrap" dangerouslySetInnerHTML={{ __html: chatbotResponse }}></div>
-         
-         </div>
-           
-          }
-            <div className="input-wrapper">
-            {!intro && <button className="regenerate-btn" onClick={regenerateResponses}>Regenrate responses</button>}
-            <div className="message-input">
-                
-            <input type="text" id="user-input" className="message-input-field" 
-              placeholder="Send message..." 
-              value={prompt} onChange={handleChange} 
-              onKeyDown={handleKeyPress}  
-              ref={promptRef} />
-              <button type="submit" onClick={handleEnter}>Send</button>
-            </div>
+                  </div>
+                </div>
+              ) : (
+                <div className="message-thread">
+                  <div className="convo-wrap" dangerouslySetInnerHTML={{ __html: chatbotResponse }}></div>
+                </div>
+              )}
+              <div className="input-wrapper">
+                {!intro && <button className="regenerate-btn" onClick={regenerateResponses}>Regenerate responses</button>}
+                <div className="message-input">
+                  <input type="text" id="user-input" className="message-input-field"
+                    placeholder="Send message..."
+                    value={prompt} onChange={handleChange}
+                    onKeyDown={handleKeyPress}
+                    ref={promptRef} />
+                  <button type="submit" onClick={handleEnter}>Send</button>
+                </div>
+              </div>
             </div>
           </div>
-  
-      </div>
-  
-  
-  
-  
-     
-  
-  {/* modal */}
-  {showModal && (
-    <Modal onClose={handleModalClose}>
-      {/* modal content goes here */}
-    </Modal>
-  )}
-     
-      </div>
-    );
-  }
-  
-  export default Chat;
+    
+         
+        </div>
+      );
+    }
+    
+    export default Chat;
--- chatgpt-poc/src/component/Chat/sidebar.js
+++ chatgpt-poc/src/component/Chat/sidebar.js
@@ -1,20 +1,86 @@
 import React, { useState } from 'react';
+import Modal from '../Modal/index';
+import user_icon from '../../assets/member.png'
+import logout from '../../assets/logout.png'
+import settings from '../../assets/settings.png'
+import chat from '../../assets/chat.png'
+import rightarrow from '../../assets/right.png'
+import downarrow from '../../assets/down.png'
+import newchat from '../../assets/newchat.png'
+import logo from '../../assets/logo.png'
+const Sidebar = ({ isShow, startNewSession, savedHistory1, chatSessions }) => {
+  const [selectedButtonIndex, setSelectedButtonIndex] = useState(null);
+    const [isopenChat , setOpenChat] = useState(false)
+    const [showModal, setShowModal] = useState(false);
+  const handleNewChat = () => {
+    setSelectedButtonIndex(null); 
+    startNewSession();
+  };
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+  const handleInfoIconClick = () => {
+    setShowModal(true);
+  };
 
-const Sidebar = ({isShow}) => {
-  
+  const handleSavedHistory = (index) => {
+    setSelectedButtonIndex(index);
+    savedHistory1(index);
+  };
+
+  const openConvo = ()=>{
+    setOpenChat(!isopenChat);
+  }
+
   return (
-    isShow ? 
+    isShow ? (
       <div className="conversation-history-card">
-        <h2>Conversation History</h2>
-        
-        <button className="history-item">
-            <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" 
-            strokeLinejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+          <h1><img src={logo} alt=""/></h1>
+         <span className="info-circle" onClick={handleInfoIconClick}>i</span>
+          <div className="user-info">
+              <img src={user_icon} alt=""/> <span>chatuser@cvs.com</span>
+          </div>
+          <div className="menu">
+              <label className="menu-label">Menu</label>
+    <a className="menu-items" onClick={openConvo}><img src={chat} alt=""/><span>Conversations</span> {isopenChat ? <img className="arrow" src={downarrow} alt=""/>: <img className="arrow" src={rightarrow} alt=""/>}</a>
+              {isopenChat && 
+              <div className="chats">
+          <button className="new-chat" onClick={handleNewChat}><img src={newchat} alt=""/>Open New Chat</button>
+            {chatSessions.map((session, index) => (
+          <button
+            className={`history-item ${selectedButtonIndex === index ? 'selected' : ''}`}
+            key={index}
+            onClick={() => handleSavedHistory(index)}
+          >
+            <svg
+              stroke="currentColor"
+              fill="none"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
             </svg>
-                Chat 1
-        </button>
-   </div> : <></>
+            Chat {index + 1}
+          </button>
+        ))}
+        </div>
+        }
+        <a className="menu-items"><img src={settings} alt=""/>Settings</a>
+              <a className="menu-items"><img src={logout} alt=""/>Logout</a>
+        </div>
+
+        {showModal && (
+            <Modal onClose={handleModalClose}>
+              {/* modal content goes here */}
+            </Modal>
+          )}
+      </div>
+    ) : null
   );
 };
 
